b4bd62c63b63c3cb319a4dd83b07c5e9dcae5918
Eros Mendonça <erosmendonca@outlook.com>
2023-11-24 02:18:00 +0000

    refactor: removing unused screens

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -8,14 +8,6 @@ import ForgetPasswordScreen from "../screens/auth/ForgetPasswordScreen";
 import UpdatePasswordScreen from "../screens/profile/UpdatePasswordScreen";
 import Tabs from "./tabs/Tabs";
 import MyAccountScreen from "../screens/profile/MyAccountScreen";
-import ProductDetailScreen from "../screens/user/ProductDetailScreen";
-import CartScreen from "../screens/user/CartScreen";
-import CheckoutScreen from "../screens/user/CheckoutScreen.js";
-import OrderConfirmScreen from "../screens/user/OrderConfirmScreen";
-
-// import MyOrderScreen from "../screens/user/MyOrderScreen";
-// import MyOrderDetailScreen from "../screens/user/MyOrderDetailScreen";
-// import CategoriesScreen from "../screens/user/CategoriesScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -33,10 +25,6 @@ const Routes = () => {
         <Stack.Screen name="updatepassword" component={UpdatePasswordScreen} />
         <Stack.Screen name="tab" component={Tabs} />
         <Stack.Screen name="myaccount" component={MyAccountScreen} />
-        <Stack.Screen name="productdetail" component={ProductDetailScreen} />
-        <Stack.Screen name="cart" component={CartScreen} />
-        <Stack.Screen name="checkout" component={CheckoutScreen} />
-        <Stack.Screen name="orderconfirm" component={OrderConfirmScreen} />
         
       </Stack.Navigator>
     </NavigationContainer>
