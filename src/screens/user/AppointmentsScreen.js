40901288d4ee11ed5d4d56ab9fbe914a7868662e
Eros Mendonça <erosmendonca@outlook.com>
2023-11-24 16:52:31 +0000

    feat: scheduling screen improvements

--- src/screens/user/AppointmentsScreen.js
+++ src/screens/user/AppointmentsScreen.js
@@ -1,4 +1,5 @@
-import { StyleSheet, View, ScrollView } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, ScrollView, TouchableOpacity } from "react-native";
 import AppointmentCard from "../../components/AppointmentCard";
 import { Text } from "react-native";
 import { getRandomDateInFuture, getRandomTomorrowDateTime } from "../../utils";
@@ -46,6 +47,18 @@ const doctorData3 = {
   profilePicture: require("../../assets/image/doctors/doctor-3.png"),
 };
 
+const filterPills = [
+  {
+    label: "Todos",
+  },
+  {
+    label: "Confirmados",
+  },
+  {
+    label: "Pendentes",
+  },
+];
+
 const generateSchedulingData = (status, customDate) => {
   const date = customDate ? customDate : getRandomDateInFuture(6);
   const day = date.getDate() + 1;
@@ -62,34 +75,56 @@ const generateSchedulingData = (status, customDate) => {
   };
 };
 
+const schedulingData = generateSchedulingData(confirmedStatus, getRandomTomorrowDateTime());
+const schedulingData2 = generateSchedulingData(pendingStatus);
+const schedulingData3 = generateSchedulingData(pendingStatus);
+const schedulingData4 = generateSchedulingData(pendingStatus);
+
 const AppointmentsScreen = () => {
+  const [activeFilter, setActiveFilter] = useState(filterPills[0].label);
+
   return (
     <View style={styles.container}>
       <ScrollView nestedScrollEnabled={true} style={{ paddingHorizontal: 10 }}>
+        <Text style={styles.mainTitle}>Agendas</Text>
+        <View style={styles.filterPills}>
+          {filterPills.map((pill, index) => (
+            <TouchableOpacity
+              key={index}
+              style={[
+                styles.filterPill,
+                activeFilter === pill.label ? {} : styles.disabledFilterPill,
+              ]}
+              onPress={() => setActiveFilter(pill.label)}
+            >
+              <Text style={styles.filterPillText}>{pill.label}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
         <Text style={styles.title}>Amanhã</Text>
         <AppointmentCard
           buttons={buttons}
           doctorData={doctorData}
-          schedulingData={generateSchedulingData(confirmedStatus, getRandomTomorrowDateTime())}
+          schedulingData={schedulingData}
         />
-
+        
         <Text style={styles.title}>Próximos</Text>
         <AppointmentCard
           buttons={rescheduleButtons}
           doctorData={doctorData2}
-          schedulingData={generateSchedulingData(pendingStatus)}
+          schedulingData={schedulingData2}
         />
 
         <AppointmentCard
           buttons={rescheduleButtons}
           doctorData={doctorData3}
-          schedulingData={generateSchedulingData(pendingStatus)}
+          schedulingData={schedulingData3}
         />
 
         <AppointmentCard
           buttons={rescheduleButtons}
           doctorData={doctorData2}
-          schedulingData={generateSchedulingData(pendingStatus)}
+          schedulingData={schedulingData4}
         />
       </ScrollView>
     </View>
@@ -112,5 +147,29 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: colors.textDark,
     marginVertical: 10,
+  },
+  mainTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: colors.textDark,
+    marginVertical: 10,
+    marginTop: 20,
+    alignSelf: "center",
+  },
+  filterPills: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 10,
+    gap: 16,
+  },
+  filterPill: {
+    backgroundColor: colors.primary,
+    borderRadius: "100%",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  disabledFilterPill: {
+    backgroundColor: colors.light,
   },
 });
